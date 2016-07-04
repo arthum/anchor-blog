07e37866cacdeeb72e0c886d4a2535bf244743c4
arthum <ah.longoria@gmail.com>
2016-07-04 18:42:09 -0500

    init design and style

--- themes/bushwick-master/footer.php
+++ themes/bushwick-master/footer.php
@@ -2,30 +2,16 @@
 
             <footer id="bottom">
             	<div class="container">
-	                <div class="grid 1of3">
-	                	<small>
-	                		<p>
-		                		&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>.<br />
-		                		All rights reserved.
-	                		</p>
-	                		<p>
-		                		Built with <a href="http://melodycss.co" target="_blank">Melody</a><br />
-		                		Powered by <a href="http://anchorcms.com" target="_blank">Anchor CMS</a>
-	                		</p>
-	                	</small>
-	                </div>
 
-					<div class="grid 1of3 offset-4">
+					<div class="grid 1of3">
 						<small>
 			                <ul role="navigation">
 			                    <li><a href="<?php echo rss_url(); ?>">RSS</a></li>
 			                    <?php if(twitter_account()): ?>
-			                    <li><a href="<?php echo twitter_url(); ?>" target="_blank">@<?php echo twitter_account(); ?></a></li>
+			                    <li><a href="https://twitter.com/arthum" target="_blank">@arthum</a></li>
 			                    <?php endif; ?>
 
 			                    <li><a href="<?php echo base_url('admin'); ?>" title="Administer your site!">Admin area</a></li>
-
-			                    <li><a href="<?php echo base_url(); ?>" title="Return to my website.">Home</a></li>
 			                </ul>
 			            </small>
 	        		</div>
